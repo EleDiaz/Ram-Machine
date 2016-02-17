9fcbcc94d1c4a2b77fa8c90edfe6a7a58feb55ba
Eleazar Díaz Delgado <eleazardzdo@gmail.com>
2016-02-17 16:47:10 +0000

    one step more

--- src/instruction.hpp
+++ src/instruction.hpp
@@ -18,40 +18,38 @@ public:
         param_(param),
         mode_(mode) {}
 
-    void run(Memory & mem, ITape & itape, OTape & otape, Program & prog) = 0;
+    virtual void run(Memory & mem, ITape & itape, OTape & otape, Program & prog) = 0;
 
-    string show(void) {
+    // string show(void) {
 
-    }
+    //}
 
-    string name(void);
+    //string name(void);
 };
-
-class
 
 
 // Carga en el 0 registro de la memoria dato
 class Load : public Instruction {
-    string show(void) { return; }
-    Load(int param, DirectionMode mode): Instruction(param,mode) {}
+    //string show(void) { return; }
+    Load(int param, Memory::DirectionMode mode): Instruction(param,mode) {}
     void run(Memory & mem, ITape & itape, OTape & otape, Program & prog) {
-        mem.setValue(0, mem.getValue(param_,mode_), Direct);
+        mem.setValue(0, mem.getValue(param_,mode_), Memory::Direct);
     }
 };
 
 class Store : public Instruction {
-    string show(void) { return; }
-    Store(int param, DirectionMode mode): Instruction(param,mode) {}
+    //string show(void) { return; }
+    Store(int param, Memory::DirectionMode mode): Instruction(param,mode) {}
     void run(Memory & mem, ITape & itape, OTape & otape, Program & prog) {
-        mem.setValue(param_, mem.getValue(0, Direct), mode_);
+        mem.setValue(param_, mem.getValue(0, Memory::Direct), mode_);
     }
 
 };
 
 
 class Read : public Instruction {
-    string show(void) { return }
-    Read(int param, DirectionMode mode): Instruction(param,mode) {}
+    //string show(void) { return }
+    Read(int param, Memory::DirectionMode mode): Instruction(param,mode) {}
     void run(Memory & mem, ITape & itape, OTape & otape, Program & prog) {
         mem.setValue(param_, itape.readTape(), mode_);
     }
@@ -59,8 +57,8 @@ class Read : public Instruction {
 };
 
 class Write : public Instruction {
-    string show(void) { return }
-    Write(int param, DirectionMode mode): Instruction(param,mode) {}
+    //string show(void) { return }
+    Write(int param, Memory::DirectionMode mode): Instruction(param,mode) {}
     void run(Memory & mem, ITape & itape, OTape & otape, Program & prog) {
         otape.writeTape(mem.getValue(param_, mode_));
     }
@@ -68,41 +66,41 @@ class Write : public Instruction {
 
 
 class Add : public Instruction {
-    string show(void) { return }
-    Add(int param, DirectionMode mode): Instruction(param,mode) {}
+    //string show(void) { return }
+    Add(int param, Memory::DirectionMode mode): Instruction(param,mode) {}
     void run(Memory & mem, ITape & itape, OTape & otape, Program & prog) {
-        mem.setValue(0, mem.getValue(param_,mode_)+mem.getValue(0,Direct), Direct);
+        mem.setValue(0, mem.getValue(param_,mode_)+mem.getValue(0,Memory::Direct), Memory::Direct);
     }
 };
 
 class Sub : public Instruction {
-    string show(void) { return }
-    Sub(int param, DirectionMode mode): Instruction(param,mode) {}
+    //string show(void) { return }
+    Sub(int param, Memory::DirectionMode mode): Instruction(param,mode) {}
     void run(Memory & mem, ITape & itape, OTape & otape, Program & prog) {
-        mem.setValue(0, mem.getValue(param_,mode_)-mem.getValue(0,Direct), Direct);
+        mem.setValue(0, mem.getValue(param_,mode_)-mem.getValue(0,Memory::Direct), Memory::Direct);
     }
 };
 
 class Mul : public Instruction {
-    string show(void) { return }
-    Mul(int param, DirectionMode mode): Instruction(param,mode) {}
+    //string show(void) { return }
+    Mul(int param, Memory::DirectionMode mode): Instruction(param,mode) {}
     void run(Memory & mem, ITape & itape, OTape & otape, Program & prog) {
-        mem.setValue(0, mem.getValue(param_,mode_)*mem.getValue(0,Direct), Direct);
+        mem.setValue(0, mem.getValue(param_,mode_)*mem.getValue(0,Memory::Direct), Memory::Direct);
     }
 };
 
 class Div : public Instruction {
-    string show(void) { return }
-    Div(int param, DirectionMode mode): Instruction(param,mode) {}
+    //string show(void) { return }
+    Div(int param, Memory::DirectionMode mode): Instruction(param,mode) {}
     void run(Memory & mem, ITape & itape, OTape & otape, Program & prog) {
-        mem.setValue(0, mem.getValue(param_,mode_) / mem.getValue(0,Direct), Direct);
+        mem.setValue(0, mem.getValue(param_,mode_) / mem.getValue(0,Memory::Direct), Memory::Direct);
     }
 };
 
 
 class Halt : public Instruction {
-    string show(void) { return }
-    Halt(int param, DirectionMode mode): Instruction(param,mode) {}
+    //string show(void) { return }
+    Halt(int param, Memory::DirectionMode mode): Instruction(param,mode) {}
     void run(Memory & mem, ITape & itape, OTape & otape, Program & prog) {
         // TODO:
     }
@@ -110,26 +108,28 @@ class Halt : public Instruction {
 };
 
 class Jump : public Instruction {
-    string show(void) { return }
-    Jump(int param, DirectionMode mode): Instruction(param,mode) {}
+    //string show(void) { return }
+    Jump(int param, Memory::DirectionMode mode): Instruction(param,mode) {}
     void run(Memory & mem, ITape & itape, OTape & otape, Program & prog) {
         //prog.moveTo(param_);
     }
 
 };
 class Jgtz : public Instruction {
-    string show(void) { return }
-    Jgtz(int param, DirectionMode mode): Instruction(param,mode) {}
+    //string show(void) { return }
+    Jgtz(int param, Memory::DirectionMode mode): Instruction(param,mode) {}
     void run(Memory & mem, ITape & itape, OTape & otape, Program & prog) {
         // TODO:
     }
 
 };
 class Jzero : public Instruction {
-    string show(void) { return }
-    Jzero(int param, DirectionMode mode): Instruction(param,mode) {}
+    //string show(void) { return }
+    Jzero(int param, Memory::DirectionMode mode): Instruction(param,mode) {}
     void run(Memory & mem, ITape & itape, OTape & otape, Program & prog) {
         // TODO:
     }
 
 };
+
+
--- src/parse.hpp
+++ src/parse.hpp
@@ -9,43 +9,43 @@
 #include "program.hpp"
 
 /**
-TODO: llevar este codigo al repo de haskell
+   TODO: llevar este codigo al repo de haskell
    tokenizer :: Stream Char → [Tokens]
    tokenizer stream = join (runParser tokenizer') . lines . stream
-     where
-       tokenizer' :: Parser [Token]
-       tokenizer' = skipWhiteSpace $ do
-         choice
-           [ TagT <$> (word >> char ':')
-           , IntructionT <$> (choice $ map string ["write", "read", "store"....])
-           , DirectT <$> many1 digit
-           , Indirect <$> char '*' >> many1 digit
-           , Indirect <$> char '=' >> many1 digit
-           ]
+   where
+   tokenizer' :: Parser [Token]
+   tokenizer' = skipWhiteSpace $ do
+   choice
+   [ TagT <$> (word >> char ':')
+   , IntructionT <$> (choice $ map string ["write", "read", "store"....])
+   , DirectT <$> many1 digit
+   , Indirect <$> char '*' >> many1 digit
+   , Indirect <$> char '=' >> many1 digit
+   ]
 */
 
 using namespace std;
 
 enum Token {
-    TagT,
-    // Instructions
-    LoadT,
-    StoreT,
-    ReadT,
-    WriteT,
-    AddT,
-    SubT,
-    MulT,
-    DivT,
-    HaltT,
-    JumpT,
-    JgztT,
-    JzeroT,
-    // Direcionamiento
-    DirectT,
-    IndirectT,
-    ImmediateT,
-    CommentT,
+  TagT,
+  // Instructions
+  LoadT,
+  StoreT,
+  ReadT,
+  WriteT,
+  AddT,
+  SubT,
+  MulT,
+  DivT,
+  HaltT,
+  JumpT,
+  JgztT,
+  JzeroT,
+  // Direcionamiento
+  DirectT,
+  IndirectT,
+  ImmediateT,
+  CommentT,
 };
 
 class Parser {
@@ -73,84 +73,108 @@ Parser::Parser(ifstream & file) {
   string line;
   while (getline(file, line)) {
     if (match_regex(line, regex("^\\s*;.*"))) // comments
-        tokens_.push_back(tuple<Token, string>(CommentT,line));
+      tokens_.push_back(tuple<Token, string>(CommentT,line));
     else {
-        smatch results;
-        if (match_regex(line, results, regex("^\\s*(\\w)\\s*:\\s*(.*)"))) {
-            tokens_.push_back(tuple<Token, string>(TagT, results[1]));
+      smatch results;
+      if (match_regex(line, results, regex("^\\s*(\\w)\\s*:\\s*(.*)"))) {
+        tokens_.push_back(tuple<Token, string>(TagT, results[1]));
 
-            if (result[2].size()>0 && !parserComments(results[2]))
-                parserInstructions(results[2]);
-        }
-        else
-            parserInstructions(line);
+        if (result[2].size()>0 && !parserComments(results[2]))
+          parserInstructions(results[2]);
       }
+      else
+        parserInstructions(line);
+    }
   }
 }
 
 bool Parser::parserComments(string str) {
-    bool temp = match_regex(str, regex("^\\s*;.*"));
-    if (temp)
-        tokens_.push_back(tuple<Token, string>(CommentT,str));
-    return temp;
+  bool temp = match_regex(str, regex("^\\s*;.*"));
+  if (temp)
+    tokens_.push_back(tuple<Token, string>(CommentT,str));
+  return temp;
 }
 
 void Parser::parserParam(string str) {
-    smatch results;
-    if (match_regex(str, results, regex("^=(\\d+)\\s*(.*)"))) {
-        tokens_.push_back(tuple<Token, string>(ImmediateT, results[1]));
-        parserComments(results[2]);
-    }
-    else if (match_regex(str, results, regex("^(\\d+)\\s*(.*)"))) {
-        tokens_.push_back(tuple<Token, string>(DirectT, result[1]));
-        parserComments(results[2]);
-    }
-    else if (match_regex(str, results, regex("^\\*(\\d+)\\s*(.*)"))) {
-        tokens_.push_back(tuple<Token, string>(IndirectT, result[1]));
-        parserComments(results[2]);
-    }
-    else if (match_regex(str, results, regex("^(\\w)\\s*(.*)"))) {
-        tokens_.push_back(tuple<Token, string>(TagT, results[1]));
-        parserComments(result[2]);
-    }
-    else
-        parserComments(str);
+  smatch results;
+  if (match_regex(str, results, regex("^=(\\d+)\\s*(.*)"))) {
+    tokens_.push_back(tuple<Token, string>(ImmediateT, results[1]));
+    parserComments(results[2]);
+  }
+  else if (match_regex(str, results, regex("^(\\d+)\\s*(.*)"))) {
+    tokens_.push_back(tuple<Token, string>(DirectT, result[1]));
+    parserComments(results[2]);
+  }
+  else if (match_regex(str, results, regex("^\\*(\\d+)\\s*(.*)"))) {
+    tokens_.push_back(tuple<Token, string>(IndirectT, result[1]));
+    parserComments(results[2]);
+  }
+  else if (match_regex(str, results, regex("^(\\w)\\s*(.*)"))) {
+    tokens_.push_back(tuple<Token, string>(TagT, results[1]));
+    parserComments(result[2]);
+  }
+  else
+    parserComments(str);
 }
 
 void Parser::parserInstructions(string str) {
-    smatch results;
+  smatch results;
 
-    auto helper = [=](string name) { return match_regex(str, results, regex("^"+name+"\\s+(.*)"));};
+  auto helper = [=](string name) { return match_regex(str, results, regex("^"+name+"\\s+(.*)"));};
 
-    auto instructionsPairs =
-        { {"LOAD", LoadT}
-        , {"STORE", StoreT}
-        , {"READ", ReadT}
-        , {"WRITE", WriteT}
-        , {"ADD", AddT}
-        , {"SUB", Sub}
-        , {"MUL", MulT}
-        , {"DIV", DivT}
-        , {"HALT", HaltT}
-        , {"JUMP", JumpT}
-        , {"JGZT", JgztT}
-        , {"JZERO", JzeroT}
-        };
+  auto instructionsPairs =
+    { {"LOAD", LoadT}
+      , {"STORE", StoreT}
+      , {"READ", ReadT}
+      , {"WRITE", WriteT}
+      , {"ADD", AddT}
+      , {"SUB", Sub}
+      , {"MUL", MulT}
+      , {"DIV", DivT}
+      , {"HALT", HaltT}
+      , {"JUMP", JumpT}
+      , {"JGZT", JgztT}
+      , {"JZERO", JzeroT}
+    };
 
 
-    for (auto i : instructionsPairs) { // TODO usar iteradores con un while
-        if (helper(i.get<0>())) {
-            tokens_.push_back(tuple<Token,string>(i.get<1>(),i.get<0>()));
-            parserParam(results[1]);
-            break;
-        }
-    }// si no se ninguna ver si es comemtario si no es un error de compilacion
+  for (auto i : instructionsPairs) { // TODO usar iteradores con un while
+    if (helper(i.get<0>())) {
+      tokens_.push_back(tuple<Token,string>(i.get<1>(),i.get<0>()));
+      parserParam(results[1]);
+      break;
+    }
+  }// si no se ninguna ver si es comemtario si no es un error de compilacion
 }
 
 
 Program Parser::getProgram(void) {
 
-  /** For tokens → tok
+  while (!tokens_.empty()) {
+    switch (tokens_front()) {
+        TagT
+        LoadT,
+        StoreT,
+        ReadT,
+        WriteT,
+        AddT,
+        SubT,
+        MulT,
+        DivT,
+        HaltT,
+        JumpT,
+        JgztT,
+        JzeroT,
+
+        DirectT,
+        IndirectT,
+        ImmediateT,
+        CommentT,
+
+    case
+    }
+  }
+    /** For tokens → tok
         yield get.format.instruction tok
-   */
+    */
 }
